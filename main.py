34cd96d0512caddcfe2e541c992370c5e475a70f
HGpk3 <nemix.danil@yandex.ru>
2023-10-16 22:00:28 +0300

    make header and footer
    make header
    make footer

--- main.py
+++ main.py
@@ -25,7 +25,9 @@ def my_render(filename, **kwargs):
 def test_page():
     return my_render("test.html", title="HELLO", name="Данил", knowledges=["JS", "SCSS", "HTML5"])
 
-
+@application.route("/base")
+def base():
+    return my_render("base.html", title="HELLO", name="Данил", knowledges=["JS", "SCSS", "HTML5"])
 @application.route("/form", methods=["GET", "POST"])
 def form_page():
     name = "Данил"
