35769da418b71f06742d879ddfd7d22a2bd6e222
Rjkzavr <kolya.toropof@gmail.com>
2023-10-18 00:19:10 +0300

    feat stud/academic/curator info at templates
    Сделал получение информации о пользователе и загрузки данных о студенте или преподавателе или кураторе

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from flask_login import LoginManager, logout_user, login_required, login_user, c
 from data import db_session
 from data.forms import FormDelete, FormLogin
 import config
+from data.users.academic import Academic
+from data.users.curator import Curator
+from data.users.student import Student
 from data.users.user import User
 
 application = Flask(__name__)
@@ -26,8 +29,16 @@ def my_render(filename, **kwargs):
         "is_authorized": current_user.is_authenticated,
         "is_logout": False,
         "user_id": current_user.id if current_user.is_authenticated else -1,
-        "current_user": current_user,
+        "user": current_user,
     }
+    if current_user.is_authenticated:
+        session = db_session.create_session()
+        if current_user.type == "student":
+            my_kwargs["student"] = session.query(Student).get(current_user.id)
+        elif current_user.type == "academic":
+            my_kwargs["academic"] = session.query(Academic).get(current_user.id)
+        elif current_user.type == "curator":
+            my_kwargs["curator"] = session.query(Curator).get(current_user.id)
     for key, val in kwargs.items():
         my_kwargs[key] = val
     return render_template(filename, **my_kwargs)
@@ -40,11 +51,13 @@ def login_page():
     if request.method == "POST":
         session = db_session.create_session()
         user = session.query(User).filter(User.email == form.email.data).first()
+        print(user)
         if user and user.check_password(form.password.data):
             login_user(user, remember=True)
             return redirect("/")
         else:
             error = "Неверная почта или пароль"
+    print(error)
     return my_render("login.html", title="Логин", form=form, error=error)
 
 
@@ -55,17 +68,22 @@ def logout():
     return redirect("/")
 
 
-@application.route("/test")
+@application.route("/test/")
 def test_page():
     return my_render("test.html", title="HELLO", name="Данил", knowledges=["JS", "SCSS", "HTML5"])
 
 
-@application.route("/base")
+@application.route("/test/profile/")
+def test_profile_page():
+    return my_render("test_profile.html", title="HELLO")
+
+
+@application.route("/base/")
 def base():
     return my_render("base.html", title="HELLO", name="Данил", knowledges=["JS", "SCSS", "HTML5"])
 
 
-@application.route("/form", methods=["GET", "POST"])
+@application.route("/form/", methods=["GET", "POST"])
 def form_page():
     name = "Данил"
     form = FormDelete()
