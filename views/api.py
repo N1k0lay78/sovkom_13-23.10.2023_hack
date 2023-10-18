7b854e29862b05e272156d2d28bab05c9198cf2d
Rjkzavr <kolya.toropof@gmail.com>
2023-10-18 22:31:03 +0300

    fix api doesnt return any value

--- views/api.py
+++ views/api.py
@@ -8,6 +8,5 @@ api_links = Blueprint('api', __name__)
 
 @api_links.route("/get_lessons/<int:group_id>/")
 def get_lesson(group_id):
-    print(get_lessons(group_id))
-    answer = {}
-    return json.encode(answer)
+    ans = get_lessons(group_id)
+    return json.encode(ans)
