62f75e4c9f5a891a7632e240b4c8aab6043f0e6e
Rjkzavr <kolya.toropof@gmail.com>
2023-10-23 19:18:35 +0300

    tmp

--- views/student.py
+++ views/student.py
@@ -3,7 +3,7 @@ from flask_login import login_required, current_user
 
 from controller.message import send, recv_messages
 from controller.student import get_lesson_of_user
-from data.forms import FormSendMessage, FormEditPassword, FormEditInfo
+from data.forms import FormSendMessage, FormEditPassword, FormEditInfo, FormLogin
 from views.tools import my_render, goto_profile
 from flask import Blueprint
 from views.tools import my_render
@@ -44,7 +44,38 @@ def info_id_page():
 @student_pages.route("/assessments/")
 @login_required
 def assessments_page():
-    return "Предметы и успеваемость"
+    user = 'Преподаватель'
+    res = {"header": ["Студент", "11.12", "12.12", "3.01", "4.01", "5.01", "6.01", "7.01", "8.01", "19.01", "20.01", "Итого"], "tabel": [{"name": "Студент Коля", "data": [[4, 1], [5, 1], [-1, 0], [3, 1], [-1, 0], [-1, 0], [-1, 1], [-1, 0], [-1, 0], [-1, -1]], "result": '-'}, {"name": "Студент Аня", "data": [[-1, 1], [-1, 1], [-1, 0], [-1, 0], [-1, 0], [3, 1], [-1, 0], [-1, 0], [-1, 0], [-1, -1]], "result": '-'},
+                                                                                                                                         {"name": "Студент Кто", "data": [[-1, 0], [5, 1], [-1, 0], [5, 1], [-1, 0], [-1, 1], [-1, 1], [-1, 1], [-1, 0], [-1, -1]], "result": '-'}, {"name": "Студент Что", "data": [[-1, 1], [4, 1], [-1, 0], [-1, 0], [-1, 1], [3, 1], [-1, 0], [-1, 1], [-1, 0], [-1, -1]], "result": '-'},
+                                                                                                                                         {"name": "Студент Когда", "data": [[4, 1], [2, 1], [-1, 0], [3, 1], [-1, 0], [-1, 0], [-1, 1], [-1, 0], [-1, 0], [-1, -1]], "result": '-'}, {"name": "Студент Где", "data": [[5, 1], [-1, 1], [5, 1], [-1, 0], [-1, 0], [-1, 1], [-1, 0], [-1, 0], [5, 1], [-1, -1]], "result": '-'}]}
+    if request.method == "POST":
+        date_form = list((dict(request.form)).values())[1:]
+        print(date_form)
+        rez = dict()
+        rez["header"] = res['header']
+        l_rez =[]
+        iter = 0
+        for i in res['tabel']:
+            d = {}
+            d['name'] = i['name']
+            list_date = []
+            print(len(res['header'][1:-1]))
+            for j in range(len(res['header'][1:-1])):
+                if date_form[j+iter] != '':
+                    list_date.append([date_form[j+iter], 1])
+                else:
+                    list_date.append(i['data'][j])
+            d['data'] = list_date
+            iter += len(res['header'][1:-1])
+            l_rez.append(d)
+        rez['tabel'] = l_rez
+        res = rez
+
+    form = FormLogin()
+    print(res)
+
+    return my_render("/student/assessment.html", title="Таблица успеваемости", header_table=res['header'][1:-1],
+                     table_len=len(res['header']), stud_data=res['tabel'], user=user, form=form)
 
 
 @student_pages.route("/assessment/<int:id>")
