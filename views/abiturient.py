3b61593def4cad525b40f6cf131719a489bb2bcf
HGpk3 <nemix.danil@yandex.ru>
2023-10-23 12:16:51 +0300

    Feat application
    Добавил заявку на регистрацию. Нужно поменять плейсхолдеры

--- views/abiturient.py
+++ views/abiturient.py
@@ -1,4 +1,6 @@
 from flask import Blueprint
+
+from data.forms import FormTest
 from views.tools import my_render
 
 abiturient_pages = Blueprint('abiturient', __name__)
@@ -7,3 +9,9 @@ abiturient_pages = Blueprint('abiturient', __name__)
 @abiturient_pages.route("/")
 def main_page():
     return my_render("/abiturient/index.html", title="")
+
+
+@abiturient_pages.route("/application")
+def application_page():
+    form = FormTest()
+    return my_render("/abiturient/application.html", title="", form=form)
